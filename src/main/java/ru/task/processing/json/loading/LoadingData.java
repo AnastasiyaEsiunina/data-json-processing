48a8ebfa2b57dbbca63afc074eb49de9fead0108
AnastasiyaEsiunina <61086630+AnastasiyaEsiunina@users.noreply.github.com>
2020-03-19 22:55:07 +0500

    Update LoadingData.java

--- src/main/java/ru/task/processing/json/loading/LoadingData.java
+++ src/main/java/ru/task/processing/json/loading/LoadingData.java
@@ -17,12 +17,11 @@ public class LoadingData implements SparkLoadable {
         Dataset<Row> main_df = spark.read().json(LoadingPath.loadingPath("path_file"));
         Dataset<Row>  main_df_registered =  main_df.filter("event=='registered'")
                 .select(col("timestamp").cast(DataTypes.TimestampType).alias("time"), col("initiator_id").cast(DataTypes.LongType), col("channel"));
-        main_df_registered.repartition(1)
+        main_df_registered
                 .write().mode(SaveMode.Overwrite).format("parquet")
                 .save(LoadingPath.loadingPath("path_datasets_reg"));
         main_df.filter("event=='app_loaded'")
                 .select(col("timestamp").cast(DataTypes.TimestampType).alias("time"), col("initiator_id").cast(DataTypes.LongType),  col("device_type"))
-                .repartition(1)
                 .write().mode(SaveMode.Overwrite).format("parquet")
                 .save(LoadingPath.loadingPath("path_datasets_app"));
     }
